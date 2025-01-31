0b902142f37f9e5154a8e58f61e26ae8ec03b6b7
GideonBauer <gideonbauer99@gmail.com>
2025-01-31 15:35:58 -0500

    Final

--- Main.py
+++ Main.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#mnist = tf.keras.datasets.mnist
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+#x_train = tf.keras.utils.normalize(x_train, axis=1)
+#x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 #model = tf.keras.models.Sequential()
 #model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
@@ -23,6 +23,19 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 #model.save('handwritten.keras')
 
 model = tf.keras.models.load_model('handwritten.keras')
-loss, accuracy = model.evaluate(x_test, y_test)
-print(loss)
-print(accuracy)
+
+image_number = 1
+
+while os.path.isfile(f"Digits/Digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"Digits/Digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"this digit is probably a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error!")
+    finally:
+        image_number += 1
+
